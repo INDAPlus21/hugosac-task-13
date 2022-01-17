a49137766edd739e34fae20f0fdc6c25756a9875
Hugo <hugosac@kth.se>
2022-01-17 20:37:43 +0100

    Tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod vector;
+use vector::Vector3;
+
 fn main() {
-    println!("Hello, world!");
+    let u: Vector3 = Vector3::new(1.0, 0.0, 0.0);
+    let v: Vector3 = Vector3::new(0.0, 1.0, 0.0);
+    println!("{}", Vector3::dot(u,v));
+    u += -u;
+    println!("{}", (u).to_string());
 }
