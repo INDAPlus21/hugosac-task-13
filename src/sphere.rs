babe047040ac0cff3c9beb424df7aff4b2449730
Hugo <hugosac@kth.se>
2022-01-19 23:12:17 +0100

    Add hit function

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,4 +1,5 @@
 use crate::vector::Vec3;
+use crate::ray::Ray;
 
 pub struct Sphere {
     center: Vec3,
@@ -11,6 +12,24 @@ impl Sphere {
             center: center,
             radius: radius
         }
+    }
+
+    pub fn center(&self) -> Vec3 {
+        self.center
+    }
+
+    pub fn radius(&self) -> f32 {
+        self.radius
+    }
+
+    pub fn hit(&self, ray: &Ray) -> bool {
+        let oc: Vec3 = ray.origin() - self.center();
+        let a: f32 = Vec3::dot(ray.direction(), ray.direction());
+        let b: f32 = 2.0 * Vec3::dot(oc, ray.direction());
+        let c: f32 = Vec3::dot(oc, oc) - self.radius() * self.radius();
+        let discriminant: f32 = b * b - 4.0 * a * c;
+        
+        discriminant > 0.0
     }
 
     pub fn to_string(&self) -> String {
