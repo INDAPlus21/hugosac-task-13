8b88d9a1d68bca20b6fa4bb6fb449060b0013f47
Hugo <hugosac@kth.se>
2022-01-21 09:02:47 +0100

    Add constants and util functions

--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,32 @@
 use rand::{Rng};
 
+use crate::vector::Vec3;
+
 pub const INFINITY: f32 = std::f32::INFINITY;
+
+pub const ASPECT_RATIO: f32 = 16.0 / 9.0;
+pub const WIDTH: i16 = 1280;
+pub const HEIGHT: i16 = (WIDTH as f32 / ASPECT_RATIO) as i16;
+pub const VIEWPORT_HEIGHT: f32 = 2.0;
+pub const VIEWPORT_WIDTH: f32 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+pub const SAMPLES: i16 = 100;
+
+pub const ORIGIN: Vec3 = Vec3 {x: 0.0, y: 0.0, z: 0.0};
 
 // Returna random float in [0, 1)
 pub fn random_f32() -> f32 {
     let mut rng = rand::thread_rng();
-    rng.gen_range(0.0, 1.0)
+    rng.gen_range(0.0..1.0)
 }
 
-// Returna random float in [min, max)
-pub fn random_in_range(min: f32, max: f32) -> f32 {
+// Return a random float in [min, max)
+pub fn random_f32_in_range(min: f32, max: f32) -> f32 {
     let mut rng = rand::thread_rng();
-    rng.gen_range(min, max)
+    rng.gen_range(min..max)
+}
+
+pub fn clamp(x: f32, min: f32, max: f32) -> f32 {
+    if x < min { return min };
+    if x > max { return max };
+    return x;
 }
